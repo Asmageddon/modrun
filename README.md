4110a1d55518639c58f2423d62a3d8d56bc7f74b
Asmageddon <asmageddon@gmail.com>
2023-08-27 14:14:40 +0200

    (Breaking) Unified default handlers and callbacks into a single system with priority, some functionality change and an updated README with better documentation

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ## The library adds the following new features:
   * An FPS limiter, disabled by default
-  * Ability to add and enable/disable event callbacks
-  * Short-circuiting subsequent callbacks, by returning true from them, e.g. to capture input to UI
+  * Ability to add and enable/disable event callbacks, including custom events.
+  * Short-circuiting subsequent callbacks, if a callback or its error handler returns `true`, e.g. to capture input to UI
   * Maintains one copy of the module only, preventing errors should multiple libraries include their own copy
 
 ## The library adds the following new events:
-  * **dispatch(event, ...)** - A dispatch event, to which all events are sent
+  * **dispatch(event, ...)** - A dispatch event, to which all events are sent. **Warning**: a custom dispatch callback with priority of -5 will run before ALL other callbacks are dispatched from the default dispatch handler, e.g. before mousepressed or update callbacks with priority -20.
   * **pre_update(dt)** - An event that runs before love.update()
   * **post_update(dt)** - An event that runs after love.update()
   * **postprocess(draw_dt)** - A second draw event, called after draw, can be used to draw overlay, profiling info, etc.
@@ -15,11 +15,62 @@
 ## The library provides the following functions:
   * **modrun.setup()** - Sets modrun up, replacing the original love.run
   * **modrun.registerEventType(event, fail_if_exists)** - Registers a new event, issues a warning, or optionally an error, if the event already exists
-  * **modrun.addCallback(event, callback, self_obj, on_error)** - Registers a new callback for an event, optionally includes an error handler, and an object to be passed as "self"
+  * **modrun.addCallback(event, callback, priority, self_obj, on_error)** - Registers a new callback for an event, optionally includes priority(`love.event` functions are 0, negative values will run earlier), an error handler, and an object to be passed as "self"
+      * **on_error(err, cb, args)** - `err` is the error, `cb` the callback that produced it, `args` are arguments passed to the callback
+      * **on_error(self_obj, err, cb, args)** - as above, but with `self_obj` passed as the `self` argument for the `function obj:fn(err, args)` format
   * **modrun.removeCallback(event, callback)** - Removes a previously registered callback
   * **modrun.enableCallback(event, callback)** - Enables a callback
   * **modrun.disableCallback(event, callback)** - Disables a callback, preventing it from being processed
   * **modrun.setFramerateLimit(fps)** - Set maximum FPS to the given value. Pass 0 or false to disable the limit. Only applies if vsync is disabled
-  * **modrun.push(event, ...)** - Push an event to the queue. Essentially same as **love.event.push()**
 
-For detailed documentation, check source
+For detailed documentation of function parameters and additional comments, check source.
+
+
+# Usage 
+
+```lua
+-- To start using the library, simply do:
+modrun = require "modrun".setup()
+
+-- Example: Handle tweening library updates before love.update is run
+modrun.addCallback("update", tween.update, -1)
+
+-- Example: Implement a state handler, and handle all events using it
+love.state = {}
+modrun.addCallback("dispatch", function(event, ...) 
+    if love.state ~= nil and love.state[event] ~= nil then 
+        return love.state[event](...)
+    end
+end)
+my_game = { update = function() end, keypressed = function() end }
+love.state = my_game
+
+-- Example: Add a callback with an error handler
+function on_error(err, cb, args) print(debug.traceback(err)) end
+modrun.addCallback("mousepressed", function() error("test error") end, nil, nil, on_error)
+
+-- Example: Add a callback for an object, so the object is passed as the `self` argument
+my_game = GameState()
+function my_game:update(dt) self.do_stuff(dt) end
+function my_game:on_error(err, cb, args) self.do_other_stuff(err) end
+-- Without error handling
+modrun.addCallback("update", GameState.update, nil, GameState)
+-- Or with error handling
+modrun.addCallback("update", GameState.update, nil, GameState, my_game.on_error)
+
+-- Example: Don't process further callbacks after returning true:
+-- Prevent quitting while the game is saving
+modrun.addCallback("quit", function() return is_game_saving() end, -99)
+-- Don't propagate events if an UI element was clicked
+function ui_handler(x, y, button, ...) 
+    if ui_element_clicked(x, y) return true end
+end
+modrun.addCallback("mousepressed", ui_handler, -99)
+```
+
+
+# Future directions
+
+ - [ ] Run dispatch inbetween hooks, so that dispatch with priority -5 dispatches mouspressed AFTER a mousepressed callback with priority -10, rather than before like now. Maybe as a separate event type to support both behaviors?
+ - [ ] Add table-of-callbacks operation for stuff like e.g. `modrun.addMultipleCallbacks(my_library.callbacks)` without having to manually add each.
+ - [ ] Add an event that handles uncaught errors in other events(`uncaughterror` maybe?) in situations where not crashing the entire game is desired.
--- modrun.lua
+++ modrun.lua
@@ -1,4 +1,4 @@
---Copyright (c) 2015 - 2022 Llamageddon <asmageddon@gmail.com>
+--Copyright (c) 2015 - 2023 Llamageddon <asmageddon@gmail.com>
 --
 --Permission is hereby granted, free of charge, to any person obtaining a
 --copy of this software and associated documentation files (the
@@ -19,27 +19,6 @@
 --TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 --SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
--- The library adds the following new features:
---  * An FPS limiter. Disabled by default
---  * Ability to add and enable/disable event callbacks
---  * Short-circuiting subsequent callbacks, by returning true from them, e.g. to capture input to UI
---  * Maintains one copy of the module only, preventing errors should multiple libraries include their own copy
---
--- The library adds the following new events:
---  * dispatch(event, ...) - A dispatch event, to which all events are sent
---  * pre_update(dt) - An event that runs before love.update()
---  * post_update(dt) - An event that runs after love.update()
---  * postprocess(draw_dt) - A second draw event, called after draw, can be used to draw overlay, profiling info, etc.
--- 
--- The library provides the following functions:
---  * modrun.setup() - Sets modrun up, replacing the original love.run
---  * modrun.registerEventType(event, fail_if_exists) - Registers a new event, issues a warning, or optionally an error, if the event already exists
---  * modrun.addCallback(event, callback, self_obj, on_error) - Registers a new callback for an event, optionally includes an error handler, and an object to be passed as "self"
---  * modrun.removeCallback(event, callback) - Removes a previously registered callback
---  * modrun.enableCallback(event, callback) - Enables a callback
---  * modrun.disableCallback(event, callback) - Disables a callback, preventing it from being processed
---  * modrun.setFramerateLimit(fps) - Set maximum FPS to the given value. Pass 0 or false to disable the limit. Only applies if vsync is disabled
-
 -- Only one instance of modrun can exist, if it is to function right
 if __modrun_singleton then
     return __modrun_singleton
@@ -56,78 +35,73 @@ local noop = function() end
 modrun.deltatime = 0
 modrun.max_fps = 0
 
-modrun.handlers = {
-    pre_quit = noop,
+-- Extend love.handlers with new(pre, post, etc.) and previously-inlined(load,update,draw) functions
+love.handlers = love.handlers or {}
 
-    pre_update = noop,
-    post_update = noop,
-    load = function(...)
-        if love.load then love.load(...) end
-    end,
-    draw = function()
-        if love.draw then love.draw() end
-    end,
-    postprocess = noop,
-    update = function(dt)
-        if love.update then love.update(dt) end
-    end,
-}
+love.handlers.pre_quit = noop
+love.handlers.pre_update = noop
+love.handlers.post_update = noop
+love.handlers.postprocess = noop
+
+love.handlers.load = function(...)
+    if love.load then love.load(...) end
+end
+love.handlers.update = function(...)
+    if love.update then love.update(...) end
+end
+love.handlers.draw = function()
+    if love.draw then love.draw() end
+end
+
 -- Dispatches events to base handlers, and calls callbacks
 -- @param event - The name of the event for which the dispatch is being handled
 -- @param ... - Arguments associated with the event
 -- @returns `true` if any of the event handlers has returned `true`, `false` otherwise
-function modrun.handlers.dispatch(event, ...)
-    local args = {...}
-    local cancel = false
 
-    if event ~= "dispatch" then
-        cancel = modrun.handlers[event](...)
-        if cancel then return true end
-        cancel = cancel or modrun.handlers.dispatch("dispatch", event, ...)
-        if cancel then return true end
-    end
+-- Dispatches the initial dispatch event for an event, which runs the handlers for both dispatch and the event itself
+function modrun._dispatchEvent(event, ...)
+    love.handlers.dispatch("dispatch", event, ...)
+end
 
-    for _, cb_entry in pairs(modrun.callbacks[event] or {}) do
-        local cb, err_handler, self_obj, enabled = cb_entry[1], cb_entry[2], cb_entry[3], cb_entry[4]
-
-        if enabled then
-            if err_handler == nil then
-                -- If no error handler was provided, we just let the error propagate down the stack
-                if self_obj then cb(self_obj, ...) else cb(...) end
+function love.handlers.dispatch(event, ...)
+    for _, cb_entry in ipairs(modrun.sorted_enabled_callbacks[event] or {}) do
+        -- local cb, err_handler, self_obj, enabled, priority = cb_entry[1], cb_entry[2], cb_entry[3], cb_entry[4], cb_entry[5]
+        local cb, enabled, priority, err_handler, self_obj = cb_entry[1], cb_entry[2], cb_entry[3], cb_entry[4], cb_entry[5]
+        local abort
+        if not enabled then
+            -- Pass, do nothing
+        elseif err_handler == nil then
+            -- If no error handler was provided, we just let the error propagate down the stack
+            if self_obj then abort = cb(self_obj, ...) else abort = cb(...) end
+        else
+            -- If an error handler was passed, handle any potential errors via it
+            local success, result
+            if self_obj ~= nil then
+                success, result = xpcall(cb, function(err) return err end, self_obj, ...)
+                abort = result
+                if not success then abort = err_handler(self_obj, result, cb, {...}) end
             else
-                -- If an error handler was passed, handle any potential errors via it
-                local success, result
-                if self_obj ~= nil then
-                    success, result = xpcall(cb, function(err) debug.traceback(err) end, self_obj, ...)
-                    if not success then
-                        err_handler(self_obj, {event, ...}, result) -- Pass traceback as well
-                    end
-                else
-                    success, result = xpcall(cb, function(err) debug.traceback(err) end, ...)
-                    if not success then
-                        err_handler({event, ...}, result) -- Pass traceback as well
-                    end
-                end
-                if success then cancel = result end
+                success, result = xpcall(cb, function(err) return err end, ...)
+                abort = result
+                if not success then abort = err_handler(result, cb, {...}) end
             end
         end
-        if cancel then return true end
+        if abort then return true end -- Short-circuit and cancel calling the rest of callbacks
     end
 
     return false
 end
--- Default to love.handlers for any handlers not specified in the table
-setmetatable(modrun.handlers, {__index = function(t, key) return rawget(love.handlers, key) end})
-
--- Each entry has the format of: { callback, on_error, self_obj, enabled }
-modrun.callbacks = {} 
+-- Each entry has the format of: { callback, enabled, priority, on_error, self_obj }
+modrun.callbacks = {}
+-- Sorted arrays of enabled callbacks for a given event
+modrun.sorted_enabled_callbacks = {}
 
 -- Register a new event type
 -- @param event - The name of the event to be registered
 -- @param fail_if_exists - (Optional) Throw an error if the event already exists
 function modrun.registerEventType(event, fail_if_exists)
-    if not modrun.handlers[event] then
-        modrun.handlers[event] = noop
+    if not love.handlers[event] then
+        love.handlers[event] = noop
         modrun.callbacks[event] = modrun.callbacks[event] or {}
     else
         if fail_if_exists then
@@ -138,52 +112,70 @@ function modrun.registerEventType(event, fail_if_exists)
     end
 end
 
+-- Behind-the-scenes function that performs callback operations, in order to have shared logic in one place.
+local callback_actions = {add = true, remove = true, enable = true, disable = true}
+function modrun._setCallback(action, event, callback, priority, self_obj, on_error)
+    error_check(callback_actions[action], "Unknown action('" .. action .. "') specified. Must be one of: add, remove, enable, disable.", 2)
+    error_check(event and rawget(love.handlers, event), "Unknown or invalid event type has been provided: '" .. tostring(event) .. "'", 2)
+    if action == "add" then
+        error_check(callback and type(callback) == "function", "No or invalid callback function has been provided: '" .. tostring(callback) .. "'", 2)
+        modrun.callbacks[event] = modrun.callbacks[event] or {}
+        modrun.callbacks[event][callback] = {callback, true, priority or 0, on_error, self_obj}
+    else
+        error_check(callback and modrun.callbacks[event][callback], "Unregistered or invalid callback has been provided", 2)
+        if action == "remove" then
+            modrun.callbacks[event][callback] = nil
+        elseif action == "enable" then
+            modrun.callbacks[event][callback][2] = true
+        elseif action == "disable" then
+            modrun.callbacks[event][callback][2] = false
+        end
+    end
+
+    -- Re-sort the enabled callbacks table for the given event
+    modrun.sorted_enabled_callbacks[event] = {}
+    for _, cb_entry in pairs(modrun.callbacks[event] or {}) do
+        if cb_entry[2] then -- If it's enabled
+            table.insert(modrun.sorted_enabled_callbacks[event], cb_entry)
+        end
+    end
+    table.sort(modrun.sorted_enabled_callbacks[event], function(a, b) return a[3] < b[3] end)
+end
+
 -- Add a callback function to be called on event
 -- If the callback returns true, further callbacks will be blocked from running
 -- @param event - The name of the event for which a callback is being registered
 -- @param callback - The callback function
+-- @param priority - Defines the order in which callbacks are executed, starting with lowest values
 -- @param self_obj - (Optional) A value to pass as the first "self" parameter to the handler
 -- @param on_error - (Optional) Error handler to invoke when the event handler throws an error
 --     The following parameters are passed to the handler: 
 --      * self_obj, if one was provided
 --      * Event info, in format of {event, ...}
 --      * The error traceback
-function modrun.addCallback(event, callback, self_obj, on_error)
-    error_check(event and modrun.handlers[event], "Unknown or invalid event type has been provided: '" .. tostring(event) .. "'")
-    error_check(callback and type(callback) == "function", "No or invalid callback function has been provided: '" .. tostring(callback) .. "'")
-    
-    modrun.callbacks[event] = modrun.callbacks[event] or {}
-    modrun.callbacks[event][callback] = {callback, on_error, self_obj, true }
+function modrun.addCallback(event, callback, priority, self_obj, on_error)
+    modrun._setCallback("add", event, callback, priority, self_obj, on_error)
 end
 
 -- Remove a callback
 -- @param event - The name of the event for which a callback is being removed
 -- @param callback - The callback function that was originally provided to modrun.addCallback
 function modrun.removeCallback(event, callback)
-    error_check(event and modrun.handlers[event], "Unknown or invalid event type has been provided: '" .. tostring(event) .. "'")
-    error_check(callback and modrun.callbacks[event][callback], "Unregistered or invalid callback has been provided")
-    
-    modrun.callbacks[event][callback] = nil
+    modrun._setCallback("remove", event, callback)
 end
 
 -- Enable a callback
 -- @param event - The name of the event for which a callback is being enabled
 -- @param callback - The callback function that was originally provided to modrun.addCallback
 function modrun.enableCallback(event, callback)
-    error_check(event and modrun.handlers[event], "Unknown or invalid event type has been provided: '" .. tostring(event) .. "'")
-    error_check(callback and modrun.callbacks[event][callback], "Unregistered or invalid callback has been provided")
-    
-    modrun.callbacks[event][callback][4] = true
+    modrun._setCallback("enable", event, callback)
 end
 
 -- Disable a callback
 -- @param event - The name of the event for which a callback is being disabled
 -- @param callback - The callback function that was originally provided to modrun.addCallback
 function modrun.disableCallback(event, callback)
-    error_check(event and modrun.handlers[event], "Unknown or invalid event type has been provided: '" .. tostring(event) .. "'")
-    error_check(callback and modrun.callbacks[event][callback], "Unregistered or invalid callback has been provided")
-    
-    modrun.callbacks[event][callback][4] = false
+    modrun._setCallback("disable", event, callback)
 end
 
 -- Set framerate limit
@@ -193,7 +185,7 @@ function modrun.setFramerateLimit(fps)
 end
 
 -- A simple function ran when love has been terminated
-function modrun.shutdown()
+function modrun._shutdown()
     if love.audio then
         love.audio.stop()
     end
@@ -201,8 +193,13 @@ end
 
 -- The run function to replace `love.run` 
 function modrun.run()
+    for event, handler in pairs(love.handlers) do
+        modrun.addCallback(event, handler, 0)
+    end
+
     if love.event then love.event.pump() end
-    modrun.handlers.dispatch("load", love.arg.parseGameArguments(arg), arg)
+    ---@diagnostic disable-next-line: undefined-field
+    modrun._dispatchEvent("load", love.arg.parseGameArguments(arg), arg)
     -- We don't want the first frame's dt to include time taken by love.load.
     if love.timer then love.timer.step() end
 
@@ -215,11 +212,11 @@ function modrun.run()
             for event, a, b, c, d, e, f in love.event.poll() do
                 -- Quit has to be handled as a special case
                 if event == "quit" then
-                    local abort_quit = modrun.handlers.dispatch(event, a, b, c, d, e, f)
-                    if not abort_quit then modrun.shutdown(); return a or 0 end
+                    local abort_quit = modrun._dispatchEvent(event, a, b, c, d, e, f)
+                    if not abort_quit then modrun._shutdown(); return a or 0 end
                 else
                     -- The rest of events can be handled normally
-                    modrun.handlers.dispatch(event,a,b,c,d,e,f) -- Does not include update or draw
+                    modrun._dispatchEvent(event,a,b,c,d,e,f) -- Does not include update or draw
                 end
             end
         end
@@ -230,16 +227,16 @@ function modrun.run()
         local before_update = love.timer.getTime()
 
         -- Call update and draw
-        modrun.handlers.dispatch("pre_update", modrun.deltatime) -- will pass 0 if love.timer is disabled
-        modrun.handlers.dispatch("update", modrun.deltatime) -- will pass 0 if love.timer is disabled
-        modrun.handlers.dispatch("post_update", modrun.deltatime) -- will pass 0 if love.timer is disabled
+        modrun._dispatchEvent("pre_update", modrun.deltatime) -- will pass 0 if love.timer is disabled
+        modrun._dispatchEvent("update", modrun.deltatime) -- will pass 0 if love.timer is disabled
+        modrun._dispatchEvent("post_update", modrun.deltatime) -- will pass 0 if love.timer is disabled
 
         if love.graphics and love.graphics.isActive() then
             love.graphics.clear(love.graphics.getBackgroundColor())
             love.graphics.origin()
             local start = love.timer.getTime()
-            modrun.handlers.dispatch("draw")
-            modrun.handlers.dispatch("postprocess", love.timer.getTime() - start)
+            modrun._dispatchEvent("draw")
+            modrun._dispatchEvent("postprocess", love.timer.getTime() - start)
             love.graphics.present()
         end
         
@@ -260,7 +257,7 @@ function modrun.setup()
 end
 
 modrun._DESCRIPTION = 'An alternative run function module for Love2D with support for callbacks and additional events'
-modrun._VERSION     = 'modrun v1.1.0'
+modrun._VERSION     = 'modrun v2.0.2'
 modrun._URL         = 'http://github.com/Asmageddon/modrun'
 modrun._LICENSE     = 'MIT LICENSE <http://www.opensource.org/licenses/mit-license.php>'
 
